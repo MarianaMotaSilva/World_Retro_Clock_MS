05fe2f131051f1d40f50ea8bfe665e13b31cfdc3
Mariana <mariana.ssousasilva@gmail.com>
2024-02-28 20:21:33 +0100

    Fixed bug in Time

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,27 @@
-function updateTime() {}
-// Barcelona
+function updateTime() {
+  // Barcelona
 
-let BarcelonaElement = document.querySelector("#Barcelona");
-let BarcelonaDateElement = BarcelonaElement.querySelector(".date");
-let BarcelonaTimeElement = BarcelonaElement.querySelector(".time");
-let BarcelonaTime = moment().tz("Europe/Madrid");
+  let BarcelonaElement = document.querySelector("#Barcelona");
+  let BarcelonaDateElement = BarcelonaElement.querySelector(".date");
+  let BarcelonaTimeElement = BarcelonaElement.querySelector(".time");
+  let BarcelonaTime = moment().tz("Europe/Madrid");
 
-BarcelonaDateElement.innerHTML = BarcelonaTime.format("MMM Do YYYY");
-BarcelonaTimeElement.innerHTML = `${BarcelonaTime.format(
-  "h:mm:ss"
-)} <small>${BarcelonaTime.format("A")}</small>`;
+  BarcelonaDateElement.innerHTML = BarcelonaTime.format("MMM Do YYYY");
+  BarcelonaTimeElement.innerHTML = `${BarcelonaTime.format(
+    "h:mm:ss"
+  )} <small>${BarcelonaTime.format("A")}</small>`;
 
-// Lisbon
+  // Lisbon
 
-let LisbonElement = document.querySelector("#Lisbon");
-let LisbonDateElement = LisbonElement.querySelector(".date");
-let LisbonTimeElement = LisbonElement.querySelector(".time");
-let LisbonTime = moment().tz("Europe/Lisbon");
+  let LisbonElement = document.querySelector("#Lisbon");
+  let LisbonDateElement = LisbonElement.querySelector(".date");
+  let LisbonTimeElement = LisbonElement.querySelector(".time");
+  let LisbonTime = moment().tz("Europe/Lisbon");
 
-LisbonDateElement.innerHTML = LisbonTime.format("MMM Do YYYY");
-LisbonTimeElement.innerHTML = `${LisbonTime.format(
-  "h:mm:ss"
-)} <small>${LisbonTime.format("A")}</small>`;
-
+  LisbonDateElement.innerHTML = LisbonTime.format("MMM Do YYYY");
+  LisbonTimeElement.innerHTML = `${LisbonTime.format(
+    "h:mm:ss"
+  )} <small>${LisbonTime.format("A")}</small>`;
+}
 updateTime();
 setInterval(updateTime, 1000);
